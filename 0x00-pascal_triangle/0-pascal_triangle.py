6dbc4d98302ffcdbfd9e2a9c1b8deec22d3cbae2
Jemeelah1 <jemiladotto@gmail.com>
2023-03-22 22:15:18 +0000

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,35 +1,27 @@
-# Python 3 code for Pascal's Triangle
-# A simple O(n^3)
-# program for
-# Pascal's Triangle
- 
-# Function to print
-# first n lines of
-# Pascal's Triangle
-def printPascal(n) :
-     
-    # Iterate through every line
-    # and print entries in it
-    for line in range(0, n) :
-         
-        # Every line has number of
-        # integers equal to line
-        # number
-        for i in range(0, line + 1) :
-            print(binomialCoeff(line, i),
-                " ", end = "")
-        print()
+#!/usr/bin/python3
+"""Pascal Triangle Interview Challenge"""
 
-def binomialCoeff(n, k) :
-    res = 1
-    if (k > n - k) :
-        k = n - k
-    for i in range(0 , k) :
-        res = res * (n - i)
-        res = res // (i + 1)
-     
-    return res
- 
-# Driver program
-n = 7
-printPascal(n)
+
+def pascal_triangle(n):
+    """returns a list of lists of numbers
+    representing the pascal triangle"""
+    if n <= 0:
+        return []
+
+    pascal_triangle = [0] * n
+
+    for i in range(n):
+        # define a row and fill first and last idx with 1
+        new_row = [0] * (i+1)
+        new_row[0] = 1
+        new_row[len(new_row) - 1] = 1
+
+        for j in range(1, i):
+            if j > 0 and j < len(new_row):
+                a = pascal_triangle[i - 1][j]
+                b = pascal_triangle[i - 1][j - 1]
+                new_row[j] = a + b
+
+        pascal_triangle[i] = new_row
+
+    return pascal_triangle
